d2af746b5f8a971deeb73f86545a880ec42ff1a2
AleksanderJu <amjalkin@gmail.com>
2022-12-06 14:06:53 +0200

    OOP_7

---
+++ src/com/company/OOP_7/Pet.java
@@ -0,0 +1,15 @@
+package com.company.OOP_7;
+
+public class Pet {
+    //Создайте класс родитель Pet.
+    //В классе добавьте поле color.
+    //Создайте конструктор, в котором задаётся цвет животного.
+
+    private String color;
+
+    public Pet(String color) {
+        this.color = color;
+    }
+
+
+}
